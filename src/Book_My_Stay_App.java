f4a7a735b53718d0df264fd8bca97af9cda05c3e
sameer-10317 <ijsameersha280605@gmail.com>
2026-03-09 09:46:21 +0530

    Use Case 1: Application Entry & Welcome Message

--- src/Book_My_Stay_App.java
+++ src/Book_My_Stay_App.java
@@ -1,2 +1,11 @@
 public class Book_My_Stay_App {
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("      Welcome to Book My Stay");
+        System.out.println("      Hotel Booking System");
+        System.out.println("           Version 1.0");
+        System.out.println("=================================");
+        System.out.println("Application Started Successfully.");
+    }
 }
